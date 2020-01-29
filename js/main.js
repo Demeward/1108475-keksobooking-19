b2be85d09d76debae27c9972857ed27943285e72
Demeward <did.dod1520@mail.ru>
2020-01-29 10:13:31 +0300

    Отображение карточки объявления

--- js/main.js
+++ js/main.js
@@ -13,6 +13,13 @@ var CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
 var CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
 var OFFERS_AMOUNT = 8;
 
+var Accommodation = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'
+};
+
 var Price = {
   MIN: 5000,
   MAX: 100000,
@@ -58,7 +65,6 @@ var generateRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-
 /**
  * Генерирует массив похожих объявлений в виде объектов
  * @param {number} amount - Количество похожих объявлений
@@ -82,11 +88,11 @@ var generateOffers = function (amount) {
         'title': 'Уютное гнездышко для молодоженов',
         'address': offerLocation.x + ', ' + offerLocation.y,
         'price': generateRandomNumber(Price.MIN, Price.MAX),
-        'type': getRandomElement(ACCOMMODATION_TYPES),
-        'rooms': getRandomElement(ROOMS),
-        'guests': getRandomElement(GUESTS),
-        'checkin': getRandomElement(CHECKIN_TIMES),
-        'checkout': getRandomElement(CHECKOUT_TIMES),
+        'type': ACCOMMODATION_TYPES[getRandomElement(ACCOMMODATION_TYPES)],
+        'rooms': ROOMS[getRandomElement(ROOMS)],
+        'guests': GUESTS[getRandomElement(GUESTS)],
+        'checkin': CHECKIN_TIMES[getRandomElement(CHECKIN_TIMES)],
+        'checkout': CHECKOUT_TIMES[getRandomElement(CHECKOUT_TIMES)],
         'features': FEATURES,
         'description': 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.',
         'photos': PHOTOS
@@ -129,3 +135,51 @@ offers.forEach(function (offerPin) {
   fragment.appendChild(renderPin(offerPin));
 });
 similarPins.appendChild(fragment);
+
+var cardTemplate = document.getElementById('card').content.querySelector('.popup');
+
+/**
+ * Задаёт на странице разметку для карточки объявления на основе шаблона и объекта данных
+ * @param {Object} offerCard - Объект с данными
+ * @return {*} Разметка карточки объявления для вставки на страницу
+*/
+var renderCard = function (offerCard) {
+
+  var cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__title').textContent = offerCard.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offerCard.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = offerCard.offer.price + 'Р/ночь';
+  cardElement.querySelector('.popup__type').textContent = Accommodation[offerCard.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = offerCard.offer.rooms + ' комнаты для ' + offerCard.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + offerCard.offer.checkin + ', выезд до ' + offerCard.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = offerCard.offer.description;
+  cardElement.querySelector('.popup__avatar').src = offerCard.author.avatar;
+
+  cardElement.querySelector('.popup__features').innerHTML = '';
+
+  offerCard.offer.features.forEach(function (feature) {
+    var cardFeature = document.createElement('li');
+    cardFeature.className = 'popup__feature ' + 'popup__feature--' + feature;
+    cardFeature.textContent = feature;
+
+    cardElement.querySelector('.popup__features').appendChild(cardFeature);
+  });
+
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+
+  offerCard.offer.photos.forEach(function (photo) {
+    var cardPhoto = document.createElement('img');
+    cardPhoto.className = 'popup__photo';
+    cardPhoto.src = photo;
+    cardPhoto.alt = 'Фотография жилья';
+    cardPhoto.style.width = 45 + 'px';
+    cardPhoto.style.height = 40 + 'px';
+
+    cardElement.querySelector('.popup__photos').appendChild(cardPhoto);
+  });
+
+  return cardElement;
+};
+
+map.appendChild(renderCard(offers[0]));
